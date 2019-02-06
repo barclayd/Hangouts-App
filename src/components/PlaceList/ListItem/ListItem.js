f4938039adf285278a8375383a21913c9c92b1cc
Daniel Barclay <daniel@urbanintel.io>
2019-02-06 01:09:33 +0000

    Output of static images using React Native <Image />

--- src/components/PlaceList/ListItem/ListItem.js
+++ src/components/PlaceList/ListItem/ListItem.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import {Text, View, StyleSheet, TouchableOpacity} from 'react-native';
+import {Text, View, StyleSheet, TouchableOpacity, Image} from 'react-native';
 
 const listItem = (props) => (
     <TouchableOpacity onPress={() => props.removeHandler(props.itemId)} onLongPress={() => alert(`Item id: ${props.itemId} was long pressed`)}>
         <View style={styles.listItem}>
             <Text style={{fontSize: 20}}>{props.placeName}</Text>
+            <Image resizeMode='cover' source={props.placeImage} style={styles.placeImage}/>
         </View>
     </TouchableOpacity>
 );
@@ -14,7 +15,15 @@ const styles = StyleSheet.create({
         width: '100%',
         padding: 20,
         backgroundColor: '#eee',
-        margin: 10
+        margin: 10,
+        flexDirection: 'row',
+        alignItems: "center",
+        justifyContent: 'space-between',
+    },
+    placeImage: {
+        marginRight: 8,
+        height: 60,
+        width: 60
     }
 });
 
