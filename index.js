990a1556c693b7d19ee85f9f48ab6195d4240e54
Daniel Barclay <barclaysd@me.com>
2019-02-08 23:58:16 +0000

    Login screen with singleScreenApp

--- index.js
+++ index.js
@@ -1,16 +1,1 @@
-import React from "react";
-import {AppRegistry} from 'react-native';
 import App from './App';
-import {Provider} from 'react-redux';
-import {name as appName} from './app.json';
-import configureStore from './src/store/configureStore';
-
-const store = configureStore();
-
-const RNRedux = () => (
-    <Provider store={store}>
-        <App />
-    </Provider>
-);
-
-AppRegistry.registerComponent(appName, () => RNRedux);
