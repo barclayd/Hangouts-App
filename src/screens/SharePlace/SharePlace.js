92545ce0012bac082399acd35dda9e99b48a9eeb
Daniel Barclay <barclaysd@me.com>
2019-02-09 00:13:47 +0000

    Tabs for Find Place and Share Place screens

---
+++ src/screens/SharePlace/SharePlace.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+
+class SharePlaceScreen extends Component {
+    render() {
+        return (
+            <View>
+                <Text>On Share Place Screen</Text>
+            </View>
+        )
+    }
+}
+
+export default SharePlaceScreen;
