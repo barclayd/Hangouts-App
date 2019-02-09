1f2e26c1072ce03a6727357ad3cb49100a792af1
Daniel Barclay <daniel@urbanintel.io>
2019-02-09 16:50:34 +0000

    Side Drawer added to app

--- src/screens/SharePlace/SharePlace.js
+++ src/screens/SharePlace/SharePlace.js
@@ -6,6 +6,21 @@ import PlaceInput from '../../components/PlaceInput/PlaceInput';
 import * as actions from '../../store/actions/index';
 
 class SharePlaceScreen extends Component {
+    constructor(props) {
+        super(props);
+        this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent);
+    }
+
+    onNavigatorEvent = (event) => {
+      console.log(event);
+      if (event.type === 'NavBarButtonPress') {
+          if(event.id === "sideDrawerToggle") {
+              this.props.navigator.toggleDrawer({
+                  side: "left"
+              });
+          }
+      }
+    };
 
     placeAddedHandler = (placeName) => {
        this.props.onAddPlace(placeName);
