0c8f075d4ea1c48c433dc3b6e086d33bf41d07aa
Daniel Barclay <daniel@urbanintel.io>
2019-02-09 00:31:18 +0000

    Icons added to tabs

--- src/screens/MainTabs/startMainTabs.js
+++ src/screens/MainTabs/startMainTabs.js
@@ -1,21 +1,33 @@
 import { Navigation } from 'react-native-navigation';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const startTabs = () => {
-    Navigation.startTabBasedApp({
-        tabs: [
-            {
-                screen: "places-app.FindPlaceScreen",
-                label: "Find Place",
-                title: "Find Place"
-            },
-            {
-                screen: "places-app.SharePlaceScreen",
-                label: "Share Place",
-                title: "Share Place"
-            }
-        ]
-    });
+    Promise.all([
+        Icon.getImageSource("ios-map", 30),
+        Icon.getImageSource("ios-people", 30)
+    ])
+        .then(sources => {
+            Navigation.startTabBasedApp({
+                tabs: [
+                    {
+                        screen: "places-app.FindPlaceScreen",
+                        label: "Find Place",
+                        title: "Find Place",
+                        icon: sources[0]
+                    },
+                    {
+                        screen: "places-app.SharePlaceScreen",
+                        label: "Share Place",
+                        title: "Share Place",
+                        icon: sources[1]
+                    }
+                ]
+            });
+        });
 };
+
+
+
 
 export default startTabs;
 
