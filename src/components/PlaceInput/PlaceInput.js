16338f7bb98000aebaf5ca96b86fdc8b2f47c729
Daniel Barclay <barclaysd@me.com>
2019-02-09 15:55:35 +0000

    Push PlaceDetail screen on pressing ListItem

--- src/components/PlaceInput/PlaceInput.js
+++ src/components/PlaceInput/PlaceInput.js
@@ -18,6 +18,9 @@ class PlaceInput extends Component {
             return;
         }
         this.props.onPlaceAdded(this.state.placeName);
+        this.setState({
+            placeName: ''
+        });
     };
 
 
--- src/screens/PlaceDetail/PlaceDetail.js
+++ src/screens/PlaceDetail/PlaceDetail.js
@@ -1,37 +1,28 @@
 import React from 'react';
-import {Modal, View, Image, Text, Button, StyleSheet, TouchableOpacity} from 'react-native';
+import {View, Image, Text, StyleSheet, TouchableOpacity} from 'react-native';
 
 import Icon from 'react-native-vector-icons/Ionicons';
 
 const placeDetail = (props) => {
-    let modalContent;
-    if (props.selectedPlace) {
-        modalContent = (
-            <View>
-                <Image source={props.selectedPlace.image} style={styles.placeImage}/>
-                <Text style={styles.placeName}>{props.selectedPlace.name}</Text>
-            </View>
-        );
-    }
     return (
-        <Modal onRequestClose={props.onModalClosed} visible={props.selectedPlace !== null} animationType="slide">
-            <View style={styles.modalContainer}>
-                {modalContent}
-            </View>
+        <View styles={styles.container}>
                 <View>
+                    <View>
+                        <Image source={props.selectedPlace.image} style={styles.placeImage} />
+                        <Text style={styles.placeName}>{props.selectedPlace.name}</Text>
+                    </View>
                     <TouchableOpacity onPress={() => props.onRemoveItem()}>
                         <View style={styles.deleteButton}>
                         <Icon size={30} color='#B33A3A' name="ios-trash"/>
                         </View>
                     </TouchableOpacity>
-                    <Button title='Close' onPress={props.onModalClosed}/>
                 </View>
-        </Modal>
-    )
+            </View>
+    );
 };
 
 const styles = StyleSheet.create({
-    modalContainer: {
+    container: {
         margin: 40
     },
     placeImage: {
