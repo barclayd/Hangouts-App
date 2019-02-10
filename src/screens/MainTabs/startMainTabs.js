5b238b610f4e210a2a2813cf4223bf6911fae9a9
Daniel Barclay <daniel@urbanintel.io>
2019-02-10 13:29:41 +0000

    Styled Side Drawer and implement dynamic styles using Platform API

--- src/screens/MainTabs/startMainTabs.js
+++ src/screens/MainTabs/startMainTabs.js
@@ -3,7 +3,7 @@ import Icon from 'react-native-vector-icons/Ionicons';
 
 const startTabs = () => {
     Promise.all([
-        Icon.getImageSource("ios-mapPicker", 30),
+        Icon.getImageSource("ios-map", 30),
         Icon.getImageSource("ios-people", 30),
         Icon.getImageSource("ios-menu", 30)
     ])
