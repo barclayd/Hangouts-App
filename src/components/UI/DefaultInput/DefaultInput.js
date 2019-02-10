78023999ce3f43cbfbd9041cebf1f695adff523c
Daniel Barclay <barclaysd@me.com>
2019-02-10 00:22:59 +0000

    Custom Components for Global Styles to implement synthetic cascading styles

---
+++ src/components/UI/DefaultInput/DefaultInput.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {TextInput, StyleSheet} from 'react-native'
+
+const defaultInput = props => (
+    <TextInput
+        {...props}
+        style={[styles.input, props.style]}
+        />
+);
+
+const styles = StyleSheet.create({
+    input: {
+        width: "100%",
+        borderWidth: 1,
+        borderColor: '#eee',
+        padding: 5,
+        margin: 8
+    }
+});
+
+
+export default defaultInput;
--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
-import {View, Text, Button} from 'react-native';
-
+import {View, Button, StyleSheet} from 'react-native';
+import DefaultInput from '../../components/UI/DefaultInput/DefaultInput';
+import HeadingText from '../../components/UI/HeadingText/HeadingText';
+import MainText from '../../components/UI/MainText/MainText';
 import startMainTabs from '../MainTabs/startMainTabs';
 
 class AuthScreen extends Component {
@@ -12,12 +14,35 @@ class AuthScreen extends Component {
     render() {
 
         return (
-            <View>
-                <Text>Auth Screen</Text>
+            <View style={styles.container}>
+                <MainText>
+                    <HeadingText>Login</HeadingText>
+                </MainText>
+                <Button title='Switch to Login' />
+                <View style={styles.inputContainer}>
+                <DefaultInput placeholder='Email' style={styles.input}/>
+                <DefaultInput placeholder='Password' style={styles.input} />
+                <DefaultInput placeholder='Confirm Password' style={styles.input}/>
+                </View>
                 <Button title='Login' onPress={this.loginHandler} />
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    inputContainer: {
+        width: '80%'
+    },
+    input: {
+        backgroundColor: '#eee',
+        borderColor: '#bbb'
+    }
+});
 
 export default AuthScreen;
