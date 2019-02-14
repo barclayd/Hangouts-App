74a3f2f178a193f8bc23167af5940e2df9231a12
Daniel Barclay <barclaysd@me.com>
2019-02-14 17:57:28 +0000

    Authentication validation feedback for user with red text input border and disabled submit button

--- src/components/UI/ButtonWithBackground/ButtonWithBackground.js
+++ src/components/UI/ButtonWithBackground/ButtonWithBackground.js
@@ -4,11 +4,13 @@ import {TouchableOpacity, TouchableNativeFeedback, StyleSheet, Text, View, Platf
 const buttonWithBackground = props => {
 
     const content = (
-        <View style={[styles.button, {backgroundColor: props.color}]}>
-            <Text>{props.children}</Text>
+        <View style={[styles.button, {backgroundColor: props.color}, props.disabled ? styles.disabled : null]}>
+            <Text style={props.disabled ? styles.disabledText : null}>{props.children}</Text>
         </View>
     );
-
+    if (props.disabled) {
+        return content;
+    }
     if (Platform.OS === 'android') {
         return (
             <TouchableNativeFeedback onPress={props.onPress}>
@@ -29,7 +31,14 @@ const styles = StyleSheet.create({
        margin: 5,
        opacity: .7,
        borderRadius: 10
-   }
+   },
+    disabled: {
+       backgroundColor: '#eee'
+    },
+    disabledText: {
+        color: '#aaa',
+        borderColor: '#aaa'
+    }
 });
 
 export default buttonWithBackground;
--- src/components/UI/DefaultInput/DefaultInput.js
+++ src/components/UI/DefaultInput/DefaultInput.js
@@ -4,7 +4,7 @@ import {TextInput, StyleSheet} from 'react-native'
 const defaultInput = props => (
     <TextInput
         {...props}
-        style={[styles.input, props.style]}
+        style={[styles.input, props.style, !props.valid && props.touched ? styles.invalid : null]}
         />
 );
 
@@ -16,6 +16,10 @@ const styles = StyleSheet.create({
         padding: 5,
         marginTop: 8,
         marginBottom: 8
+    },
+    invalid: {
+        backgroundColor: '#f9c0c0',
+        borderColor: 'red'
     }
 });
 
