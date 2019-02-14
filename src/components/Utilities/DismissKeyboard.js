dfe339232f6d3cd96f039a1a46a23f060a3a0c38
Daniel Barclay <daniel@urbanintel.io>
2019-02-14 20:22:46 +0000

    Dismiss keyboard HOC

---
+++ src/components/Utilities/DismissKeyboard.js
@@ -0,0 +1,20 @@
+import {Keyboard, TouchableWithoutFeedback, View, StyleSheet} from "react-native";
+import React from "react";
+
+export const DismissKeyboard = ({ children }) => (
+        <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+            <View style={styles.container}>
+            {children}
+            </View>
+        </TouchableWithoutFeedback>
+);
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    }
+});
+
+
+
+
