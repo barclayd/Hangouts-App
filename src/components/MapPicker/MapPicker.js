a1c299a073dc572d88b421998cfbcf7275bcb931
Daniel Barclay <daniel@urbanintel.io>
2019-02-16 00:01:41 +0000

    Pick a location on the map, animate to picked location and place a map marker

--- src/components/MapPicker/MapPicker.js
+++ src/components/MapPicker/MapPicker.js
@@ -10,16 +10,44 @@ class MapPicker extends Component {
             longitude: -3.1791,
             latitudeDelta: 0.0122,
             longitudeDelta: Dimensions.get('window').width / Dimensions.get('window').height * 0.122
-        }
+        },
+        locationChosen: false
+    };
+
+    pickLocationHandler = event => {
+        const coords = event.nativeEvent.coordinate;
+        this.map.animateToRegion({
+           ...this.state.focusedLocation,
+           latitude: coords.latitude,
+            longitude: coords.longitude
+        });
+        this.setState(prevState => {
+            return {
+                focusedLocation: {
+                    ...prevState.focusedLocation,
+                    latitude: coords.latitude,
+                    longitude: coords.longitude
+                },
+                locationChosen: true
+            }
+        })
     };
 
     render() {
+        let marker = null;
+        if (this.state.locationChosen) {
+            marker = <MapView.Marker coordinate={this.state.focusedLocation}/>
+        }
         return (
             <View style={styles.container}>
                 <MapView
                     initialRegion={this.state.focusedLocation}
                     style={styles.map}
-                />
+                    onPress={this.pickLocationHandler}
+                    ref={ref => this.map = ref}
+                >
+                    {marker}
+                </MapView>
                 <View style={styles.button}>
                     <Button title="Locate me" onPress={() => alert('Pick location!')}/>
                 </View>
