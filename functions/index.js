345778df457d55d07edd4a52e741b5a31257de4e
Daniel Barclay <daniel@urbanintel.io>
2019-02-21 23:21:24 +0000

    Store place data includes link to image stored in Firebase storage

--- functions/index.js
+++ functions/index.js
@@ -3,33 +3,45 @@ const cors = require('cors')({origin: true});
 const fs = require('fs');
 const UUID = require('uuid-v4');
 
-const gcconfig = {
-    projectId: "places-app-1550271704119",
-    keyFilename: "places.json"
-};
+const {Storage} = require('@google-cloud/storage');
 
-const gcs = require('@google-cloud/storage')(gcconfig);
+const storage = new Storage({
+    projectId: "places-app-1550271704119",
+    keyFilename: "./places.json"
+});
 
 exports.storeImage = functions.https.onRequest((request, response) => {
     return cors(request, response, () => {
        // extract image
         const body = JSON.parse(request.body);
-        fs.writeFileSync("/tmp/uploadedImage.jpg", body.image, "base64", err => {
+        fs.writeFileSync("/tmp/uploaded-image.jpg", body.image, "base64", err => {
             console.log(err);
             return response.status(500).json({err: err});
         });
 
+        const bucket = storage.bucket("places-app-1550271704119.appspot.com");
         const uuid = UUID();
 
         // store image on Firebase Storage
-        const bucket = gcs.bucket("places-app-1550271704119.appspot.com");
-        return bucket.upload("/tmp/uploadedImage.jpg", {
+        return bucket.upload("/tmp/uploaded-image.jpg", {
            uploadType: "media",
            destination: `/places/${uuid}.jpg`,
             metadata: {
-               contentType: "image/jpg",
-                firebaseStorageDownloadTokens: uuid
+               metadata: {
+                   contentType: "image/jpeg",
+                   firebaseStorageDownloadTokens: uuid
+               }
             }
+        }, (err, file) => {
+                if(!err) {
+                    return response.status(201).json({
+                        imageUrl: `https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/${encodeURIComponent(file.name)}?alt=media&token=${uuid}`
+                    });
+                } else {
+                    console.log('function failed');
+                    console.log(err);
+                    return response.status(500).json({error: err});
+                }
         });
     });
 });
