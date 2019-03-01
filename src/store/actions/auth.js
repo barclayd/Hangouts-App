69e6257f33a3ea1961a66660e0c854e396a757a0
Daniel Barclay <daniel@urbanintel.io>
2019-03-01 23:04:58 +0000

    User login supported with sign in logic

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,9 +1,13 @@
 import * as actions from './index';
 import startMainTabs from '../../screens/MainTabs/startMainTabs';
 
-export const tryAuth  = (authData) => {
+export const tryAuth  = (authData, authMode) => {
    return dispatch => {
-     dispatch(authSignUp(authData));
+       if (authMode === 'login') {
+           dispatch(authLogin(authData));
+       } else {
+           dispatch(authSignUp(authData));
+       }
    };
 };
 
@@ -32,6 +36,44 @@ export const authSignUp = (authData) => {
                 if (parsedRes.error) {
                     alert(`Authentication failed due to ${parsedRes.error.message}. Please try again`);
                 } else {
+                    console.log('new account created');
+                    startMainTabs();
+                }
+            })
+            .catch(err => {
+                console.log(err);
+                alert('Authentication connection failed. Please try again');
+                dispatch(actions.uiStopLoading());
+            });
+    };
+};
+
+export const authLogin = (authData) => {
+    return dispatch => {
+        dispatch(actions.uiStartLoading());
+        fetch(`https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyCPS1KNVPhI5IxJv63V-4SVK9G_RhDu4lI`, {
+            method: "POST",
+            body: JSON.stringify({
+                email: authData.email,
+                password: authData.password,
+                returnSecureToken: true
+            }),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+            .catch(err => {
+                console.log(err);
+                alert('Authentication failed. Please try again');
+                dispatch(actions.uiStopLoading());
+            })
+            .then(res => res.json())
+            .then(parsedRes => {
+                dispatch(actions.uiStopLoading());
+                if (parsedRes.error) {
+                    alert(`Authentication failed due to ${parsedRes.error.message}. Please try again`);
+                } else {
+                    console.log('logged in');
                     startMainTabs();
                 }
             })
