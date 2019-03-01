ca8ca8772b19a2ce84015b3bb73d871bc58f65b3
Daniel Barclay <daniel@urbanintel.io>
2019-03-01 21:31:37 +0000

    User sign up connects and validates with Firebase Auth API

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,8 +1,44 @@
-import * as actionTypes from './actionTypes';
+import * as actions from './index';
+import startMainTabs from '../../screens/MainTabs/startMainTabs';
 
 export const tryAuth  = (authData) => {
-    return {
-      type: actionTypes.TRY_AUTH,
-      authData: authData
+   return dispatch => {
+     dispatch(authSignUp(authData));
+   };
+};
+
+export const authSignUp = (authData) => {
+    return dispatch => {
+        dispatch(actions.uiStartLoading());
+        fetch(`https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyCPS1KNVPhI5IxJv63V-4SVK9G_RhDu4lI`, {
+            method: "POST",
+            body: JSON.stringify({
+                email: authData.email,
+                password: authData.password,
+                returnSecureToken: true
+            }),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+            .catch(err => {
+                console.log(err);
+                alert('Authentication failed. Please try again');
+                dispatch(actions.uiStopLoading());
+            })
+            .then(res => res.json())
+            .then(parsedRes => {
+                dispatch(actions.uiStopLoading());
+                if (parsedRes.error) {
+                    alert(`Authentication failed due to ${parsedRes.error.message}. Please try again`);
+                } else {
+                    startMainTabs();
+                }
+            })
+            .catch(err => {
+                console.log(err);
+                alert('Authentication connection failed. Please try again');
+                dispatch(actions.uiStopLoading());
+            });
     };
 };
